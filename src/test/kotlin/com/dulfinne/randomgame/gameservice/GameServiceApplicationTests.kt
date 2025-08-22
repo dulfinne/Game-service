c46802a6e382266cd10eb06c7cfd476ef6fa9b1f
yani <yanazhuchok@gmail.com>
2025-08-22 15:48:15 +0300

    Initial commit

---
+++ src/test/kotlin/com/dulfinne/randomgame/gameservice/GameServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.dulfinne.randomgame.gameservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GameServiceApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
