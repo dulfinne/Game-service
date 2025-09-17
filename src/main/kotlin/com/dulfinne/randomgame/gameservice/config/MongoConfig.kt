05b714f2ed3c473ac2b5dc682c04998ef0bc5362
yani <yanazhuchok@gmail.com>
2025-09-17 15:26:39 +0300

    fix: add transaction manager

---
+++ src/main/kotlin/com/dulfinne/randomgame/gameservice/config/MongoConfig.kt
@@ -0,0 +1,14 @@
+package com.dulfinne.randomgame.gameservice.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.mongodb.ReactiveMongoDatabaseFactory
+import org.springframework.data.mongodb.ReactiveMongoTransactionManager
+
+@Configuration
+class MongoConfig {
+
+    @Bean
+    fun transactionManager(dbFactory: ReactiveMongoDatabaseFactory): ReactiveMongoTransactionManager =
+        ReactiveMongoTransactionManager(dbFactory)
+}
